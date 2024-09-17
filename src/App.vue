82ff7e975b05845a9bab0f49cb5bc2f4cdbf95a8
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-09-17 22:23:49 +0200

    Milestone 0-1

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,86 @@
 <script>
-/* 
-  Per importare ed utilizzare un componente dentro un altro devo SEMPRE seguire questi 3 passi:
-  1) Importazione del componente
-  2) Dichiarazione del componente
-  3) Utilizzo del componente
-*/
-// 1) Importazione del componente
-import AppFooter from './components/AppFooter.vue';
-import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
+import axios from 'axios';
 
 export default {
   data() {
     return { 
+      apikey: '3a4db1cb907349cc970d7a7e93db75c6',
+      searchText: '',
+      movies:[]
       
     }
   },
-  // 2) Dichiarazione del componente
-  components: {
-    AppHeader,
-    AppMain,
-    AppFooter
-  },
   methods: {
-    
+    search() {
+      console.log(this.searchText);  
+
+      axios
+      .get('https://api.themoviedb.org/3/search/movie',{
+        params:{
+          api_key: this.apikey,
+          query: this.searchText,
+
+        }
+      })
+      .then((resp) => {
+        console.log(resp.data);
+
+        this.movies = resp.data.results;
+      });
+        
+        
+      }
+    }  
   }
-}
+
 </script>
 
 <template>
-  <div>
-    <!-- 3) Utilizzo del componente -->
-    <AppHeader/>
-    <AppMain/>
-    <AppFooter/>
+  <div class="">
+    <div class="container py-5">
+      <div class="d-flex justify-content-between">
+        <form @submit.prevent="search">
+          <input v-model="searchText" type="text" placeholder="Cerca film o serie TV..">
+          <button type="submit">
+            Cerca
+          </button>
+        </form>
+
+      </div>
+    </div>
+    <div>
+      <ol>
+        <li v-for="(movie, i) in movies" :key="i">
+          <ul>
+            <li>
+              {{ movie.poster_path }}
+            </li>
+            <li>
+              Titolo: {{ movie.title }}
+            </li>
+            <li>
+              Titolo originale: {{ movie.original_title }}
+            </li>
+            <li>
+              Lingua:{{ movie.original_language }}
+            </li>
+            <li>
+              Voto:{{ movie.vote_average }}
+            </li>
+          </ul>
+          <hr>
+        </li>
+      </ol>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+.BG{
+  background-color: black;
+  color: white;
+  height: 100vh;
+}
 
 // Import all of Bootstrap's CSS
 @import "bootstrap/scss/bootstrap";
